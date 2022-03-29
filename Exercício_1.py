7a24abe3b81e73e3f73315733dec6d97b4e7af45
WillSols <101078851+WillSols@users.noreply.github.com>
2022-03-29 00:52:58 -0300

    Add files via upload

---
+++ Exercício_1.py
@@ -0,0 +1,5 @@
+# Crie um programa que receba uma palavra ou frase e mostre o texto invertido. 
+# Exemplo: "programação" deve aparecer como "oãçamargorp
+texto = " Programação"
+
+print(texto[::-1])
